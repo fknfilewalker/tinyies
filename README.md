aafc60b383482fb896549fd2aed1fff795d301e7
Lukas Lipp <llipp@cg.tuwien.ac.at>
2021-10-02 11:16:58 +0200

    release fixes

--- README.md
+++ README.md
@@ -3,22 +3,12 @@ A simple header only loader for ies files. Parses the file and writes its conten
 
 ## Usage
 ```c++
-tiny_ies::Light ies;
-std::string err;
-if (!tiny_ies::load_IES(file, err, ies)) {
-    // print err
-}
-```
+#include <tiny_ies.hpp>
 
-Upload the candela array to an image of size `#vertical_angles x #horizontal_angles`. Use a sampler and set wrap_s to `CLAMP_TO_BORDER` and wrap_v to `MIRRORED_REPEAT`.
-```glsl
-float ies(vec3 n, vec3 t, vec3 dir){
-    const float v_angle = radians(acos(dot(n, dir)));
-    const float h_angle = radians(acos(dot(t, dir)));
-    vec2 uv;
-    uv.x = (v_angle - v_min_angle) / (v_max_angle - v_min_angle);
-    uv.y = (h_angle - h_min_angle) / (h_max_angle - h_min_angle);
-    return texture(ies_sampler, uv).r;
+tiny_ies::light ies;
+std::string err;
+if (!tiny_ies::load_ies(file.string(), err, ies)) {
+	// print err
 }
 ```
 
@@ -26,24 +16,3 @@ float ies(vec3 n, vec3 t, vec3 dir){
 * external tile file aka `TILT=<filename>`
 
 [License (MIT)](https://github.com/fknfilewalker/tinyies/blob/main/LICENSE)
--------------------------------------------------------------------------------
-	Copyright (C) 2021 fknfilewalker. All rights reserved.
-
-	https://github.com/fknfilewalker/tinyies
-
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included
-	in all copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-	BRIAN PAUL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-	AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- tiny_ies.hpp
+++ tiny_ies.hpp
@@ -1,5 +1,42 @@
 #pragma once
 
+// +----------------------------------------------------------------------
+// | Project : tinyies.
+// | All rights reserved.
+// +----------------------------------------------------------------------
+// | Copyright (c) 2021.
+// +----------------------------------------------------------------------
+// | * Redistribution and use of this software in source and binary forms,
+// |   with or without modification, are permitted provided that the following
+// |   conditions are met:
+// |
+// | * Redistributions of source code must retain the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer.
+// |
+// | * Redistributions in binary form must reproduce the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer in the documentation and/or other
+// |   materials provided with the distribution.
+// |
+// | * Neither the name of the ray team, nor the names of its
+// |   contributors may be used to endorse or promote products
+// |   derived from this software without specific prior
+// |   written permission of the ray team.
+// |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// +----------------------------------------------------------------------
+
 #include <string>
 #include <map>
 #include <vector>
@@ -8,7 +45,27 @@
 
 class tiny_ies {
 public:
-    struct Light {
+    struct light {
+        light() : lamp_to_luminaire_geometry(),
+    			  number_of_tilt_angles(0),
+    			  number_lights(0),
+    			  lumens_per_lamp(0),
+                  multiplier(0),
+                  number_vertical_angles(0),
+                  number_horizontal_angles(0),
+                  photometric_type(0), units_type(0),
+                  width(0), length(0), height(0),
+                  ballast_factor(0),
+                  future_use(0),
+                  input_watts(0),
+                  min_vertical_angle(0),
+                  max_vertical_angle(0),
+                  min_horizontal_angle(0),
+                  max_horizontal_angle(0),
+                  max_candela(0)
+        {
+        }
+
         /*
         ** HEADER
         */
@@ -42,30 +99,34 @@ public:
         float input_watts;
 
         std::vector<float> vertical_angles;
+        float min_vertical_angle;
+        float max_vertical_angle;
         std::vector<float> horizontal_angles;
+        float min_horizontal_angle;
+        float max_horizontal_angle;
 
         std::vector<float> candela;
         float max_candela;
     };
 
-    static bool load_IES(const std::string file, std::string& err_out, Light& ies_out) {
+    static bool load_ies(const std::string file, std::string& err_out, light& ies_out) {
         std::ifstream f(file);
         if (!f) {
             err_out = "Failed reading file: " + file;
             return false;
         }
 
-        ies_out = Light();
+        ies_out = light();
         std::string line;
         /*
         ** HEADER
         */
-        // the first line in a valid ies file needs to be IESNA:
+        // the first line in a valid ies file should be IESNA:
         if (f.good()) {
             std::getline(f, line);
             if (!read_property("IESNA:", line, ies_out.ies_version)) {
                 err_out = "First line did not start with IESNA: " + file;
-                return false;
+                //return false;
             }
         }
 
@@ -82,13 +143,21 @@ public:
         /*
         ** DATA
         */
-        std::stringstream buffer;
-        buffer << f.rdbuf();
+        // replace comma with empty space
+        std::ostringstream oss;
+        oss << f.rdbuf();
+        std::string s = oss.str();
+        std::replace(s.begin(), s.end(), ',', ' ');
+
+        // go through the data
+    	std::stringstream buffer;
+        buffer.str(s);
         float value;
+
 #define NEXT_FLOAT(name) if (!(buffer >> value)) { err_out = "Error reading <" name "> property: " + file; f.close(); return false; }
 
         // <lamp to luminaire geometry> <#tilt angles> <angles> <multiplying factors>
-        if (!ies_out.tilt.compare("INCLUDE")) {
+        if (ies_out.tilt == "INCLUDE") {
             NEXT_FLOAT("lamp to luminaire geometry")    ies_out.lamp_to_luminaire_geometry = static_cast<int>(value);
             NEXT_FLOAT("#tilt angles")                  ies_out.number_of_tilt_angles = static_cast<int>(value);
             ies_out.tilt_angles.reserve(ies_out.number_of_tilt_angles);
@@ -101,7 +170,7 @@ public:
             }
         }
 
-        // <#lamps> <lumens/lamp> <multiplier> <#vertical angles> <#horizontal angles> <photometric type> <units type> <width> <length> <height>
+        // <#lamps> <lumen/lamp> <multiplier> <#vertical angles> <#horizontal angles> <photometric type> <units type> <width> <length> <height>
         NEXT_FLOAT("#lamps")                ies_out.number_lights = static_cast<int>(value);
         NEXT_FLOAT("lumens/lamp")           ies_out.lumens_per_lamp = static_cast<int>(value);
         NEXT_FLOAT("multiplier")            ies_out.multiplier = value;
@@ -117,19 +186,27 @@ public:
         NEXT_FLOAT("future use")            ies_out.future_use = value;
         NEXT_FLOAT("input watts")           ies_out.input_watts = value;
         // <vertical angles>
+        ies_out.min_vertical_angle = 360;
+        ies_out.max_vertical_angle = -360;
         ies_out.vertical_angles.reserve(ies_out.number_vertical_angles);
         for (int i = 0; i < ies_out.number_vertical_angles; i++) {
             NEXT_FLOAT("vertical angles")   ies_out.vertical_angles.push_back(value);
+            if (value < ies_out.min_vertical_angle) ies_out.min_vertical_angle = value;
+            if (value > ies_out.max_vertical_angle) ies_out.max_vertical_angle = value;
         }
         // <horizontal angles>
+        ies_out.min_horizontal_angle = 360;
+        ies_out.max_horizontal_angle = -360;
         ies_out.horizontal_angles.reserve(ies_out.number_horizontal_angles);
         for (int i = 0; i < ies_out.number_horizontal_angles; i++) {
             NEXT_FLOAT("horizontal angles") ies_out.horizontal_angles.push_back(value);
+            if (value < ies_out.min_horizontal_angle) ies_out.min_horizontal_angle = value;
+            if (value > ies_out.max_horizontal_angle) ies_out.max_horizontal_angle = value;
         }
         // <candela values for all vertical angles at first horizontal angle>
         //                                              :
         // <candela values for all vertical angles at last horizontal angle>
-        ies_out.candela.reserve((uint64_t)ies_out.number_vertical_angles * (uint64_t)ies_out.number_horizontal_angles);
+        ies_out.candela.reserve(static_cast<uint64_t>(ies_out.number_vertical_angles) * static_cast<uint64_t>(ies_out.number_horizontal_angles));
         ies_out.max_candela = 0;
         for (int i = 0; i < ies_out.number_vertical_angles * ies_out.number_horizontal_angles; i++) {
             NEXT_FLOAT("candela values")    ies_out.candela.push_back(value);
@@ -142,20 +219,22 @@ public:
 
 private:
     /* read property in current line and return value */
-    static inline bool read_property(const std::string name, std::string& line, std::string& out) {
-        int pos = line.find(name);
+    static inline bool read_property(const std::string& name, std::string& line, std::string& out) {
+        size_t pos = line.find(name);
         if (pos == std::string::npos) return false;
 
-        std::string substring = line.substr(pos + name.size(), line.size());
-        out = substring.substr(substring.find_first_not_of(" "), substring.size());
+        const std::string substring = line.substr(pos + name.size(), line.size());
+        pos = substring.find_first_not_of(' ');
+        if (pos == std::string::npos) return false;
+        out = substring.substr(pos, substring.size());
         return true;
     }
     /* read property in current line and add it to the dict */
     static inline bool read_property(std::string& line, std::map<std::string, std::string>& property_map) {
         const std::string start_delim = "[";
         const std::string stop_delim = "]";
-        size_t  attribute_start = line.find(start_delim) + start_delim.size();
-        size_t  attribute_end = line.find_last_of(stop_delim);
+        const size_t attribute_start = line.find(start_delim) + start_delim.size();
+        const size_t attribute_end = line.find_last_of(stop_delim);
         if (attribute_start == std::string::npos || attribute_end == std::string::npos) return false;
 
         std::string attribute = line.substr(attribute_start, attribute_end - attribute_start);
