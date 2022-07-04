4adf3494102f452c42682b22edee074d01bb30a1
Lukas Lipp <llipp@cg.tuwien.ac.at>
2022-07-04 22:42:32 +0200

    set precision

--- tiny_ies.hpp
+++ tiny_ies.hpp
@@ -2,7 +2,7 @@
 // | Project : tinyies.
 // | All rights reserved.
 // +----------------------------------------------------------------------
-// | Copyright (c) Lukas Lipp 2021.
+// | Copyright (c) Lukas Lipp 2021-2022.
 // +----------------------------------------------------------------------
 // | * Redistribution and use of this software in source and binary forms,
 // |   with or without modification, are permitted provided that the following
@@ -42,6 +42,12 @@
 #include <fstream>
 #include <sstream>
 
+#ifdef TINYIES_USE_DOUBLE
+#define TINYIES_FPN double
+#else
+#define TINYIES_FPN float
+#endif
+
 class tiny_ies {
 public:
     struct light {
@@ -78,34 +84,34 @@ public:
         /* tilt data */
         int lamp_to_luminaire_geometry;
         int number_of_tilt_angles;
-        std::vector<float> tilt_angles;
-        std::vector<float> tilt_multiplying_factors;
+        std::vector<TINYIES_FPN> tilt_angles;
+        std::vector<TINYIES_FPN> tilt_multiplying_factors;
 
         /* data */
         int number_lights;
         int lumens_per_lamp;
-        float multiplier;
+        TINYIES_FPN multiplier;
         int number_vertical_angles;
         int number_horizontal_angles;
         int photometric_type;
         int units_type;
-        float width;
-        float length;
-        float height;
+        TINYIES_FPN width;
+        TINYIES_FPN length;
+        TINYIES_FPN height;
 
-        float ballast_factor;
-        float future_use;
-        float input_watts;
+        TINYIES_FPN ballast_factor;
+        TINYIES_FPN future_use;
+        TINYIES_FPN input_watts;
 
-        std::vector<float> vertical_angles;
-        float min_vertical_angle;
-        float max_vertical_angle;
-        std::vector<float> horizontal_angles;
-        float min_horizontal_angle;
-        float max_horizontal_angle;
+        std::vector<TINYIES_FPN> vertical_angles;
+        TINYIES_FPN min_vertical_angle;
+        TINYIES_FPN max_vertical_angle;
+        std::vector<TINYIES_FPN> horizontal_angles;
+        TINYIES_FPN min_horizontal_angle;
+        TINYIES_FPN max_horizontal_angle;
 
-        std::vector<float> candela;
-        float max_candela;
+        std::vector<TINYIES_FPN> candela;
+        TINYIES_FPN max_candela;
     };
 
     static bool load_ies(const std::string& file, std::string& err_out, std::string& warn_out, light& ies_out) {
@@ -151,45 +157,46 @@ public:
         // go through the data
     	std::stringstream buffer;
         buffer.str(s);
-        float value;
 
-#define NEXT_FLOAT(name) if (!(buffer >> value)) { err_out = "Error reading <" name "> property: " + file; f.close(); return false; }
+        TINYIES_FPN value;
+#define NEXT_VALUE(name) if (!(buffer >> value)) { err_out = "Error reading <" name "> property: " + file; f.close(); return false; }
 
         // <lamp to luminaire geometry> <#tilt angles> <angles> <multiplying factors>
         if (ies_out.tilt == "INCLUDE") {
-            NEXT_FLOAT("lamp to luminaire geometry")    ies_out.lamp_to_luminaire_geometry = static_cast<int>(value);
-            NEXT_FLOAT("#tilt angles")                  ies_out.number_of_tilt_angles = static_cast<int>(value);
+            NEXT_VALUE("lamp to luminaire geometry")    ies_out.lamp_to_luminaire_geometry = static_cast<int>(value);
+            NEXT_VALUE("#tilt angles")                  ies_out.number_of_tilt_angles = static_cast<int>(value);
+
             ies_out.tilt_angles.reserve(ies_out.number_of_tilt_angles);
             ies_out.tilt_multiplying_factors.reserve(ies_out.number_of_tilt_angles);
             for (int i = 0; i < ies_out.number_of_tilt_angles; i++) {
-                NEXT_FLOAT("angles")        ies_out.tilt_angles.push_back(value);
+                NEXT_VALUE("angles")        ies_out.tilt_angles.push_back(value);
             }
             for (int i = 0; i < ies_out.number_of_tilt_angles; i++) {
-                NEXT_FLOAT("angles")        ies_out.tilt_multiplying_factors.push_back(value);
+                NEXT_VALUE("angles")        ies_out.tilt_multiplying_factors.push_back(value);
             }
         }
 
         // <#lamps> <lumen/lamp> <multiplier> <#vertical angles> <#horizontal angles> <photometric type> <units type> <width> <length> <height>
-        NEXT_FLOAT("#lamps")                ies_out.number_lights = static_cast<int>(value);
-        NEXT_FLOAT("lumens/lamp")           ies_out.lumens_per_lamp = static_cast<int>(value);
-        NEXT_FLOAT("multiplier")            ies_out.multiplier = value;
-        NEXT_FLOAT("#vertical angles")      ies_out.number_vertical_angles = static_cast<int>(value);
-        NEXT_FLOAT("#horizontal angles")    ies_out.number_horizontal_angles = static_cast<int>(value);
-        NEXT_FLOAT("photometric type")      ies_out.photometric_type = static_cast<int>(value);
-        NEXT_FLOAT("units type")            ies_out.units_type = static_cast<int>(value);
-        NEXT_FLOAT("width")                 ies_out.width = value;
-        NEXT_FLOAT("length")                ies_out.length = value;
-        NEXT_FLOAT("height")                ies_out.height = value;
+        NEXT_VALUE("#lamps")                ies_out.number_lights = static_cast<int>(value);
+        NEXT_VALUE("lumens/lamp")           ies_out.lumens_per_lamp = static_cast<int>(value);
+        NEXT_VALUE("multiplier")            ies_out.multiplier = value;
+        NEXT_VALUE("#vertical angles")      ies_out.number_vertical_angles = static_cast<int>(value);
+        NEXT_VALUE("#horizontal angles")    ies_out.number_horizontal_angles = static_cast<int>(value);
+        NEXT_VALUE("photometric type")      ies_out.photometric_type = static_cast<int>(value);
+        NEXT_VALUE("units type")            ies_out.units_type = static_cast<int>(value);
+        NEXT_VALUE("width")                 ies_out.width = value;
+        NEXT_VALUE("length")                ies_out.length = value;
+        NEXT_VALUE("height")                ies_out.height = value;
         // <ballast factor> <future use> <input watts>
-        NEXT_FLOAT("ballast factor")        ies_out.ballast_factor = value;
-        NEXT_FLOAT("future use")            ies_out.future_use = value;
-        NEXT_FLOAT("input watts")           ies_out.input_watts = value;
+        NEXT_VALUE("ballast factor")        ies_out.ballast_factor = value;
+        NEXT_VALUE("future use")            ies_out.future_use = value;
+        NEXT_VALUE("input watts")           ies_out.input_watts = value;
         // <vertical angles>
         ies_out.min_vertical_angle = 360;
         ies_out.max_vertical_angle = -360;
         ies_out.vertical_angles.reserve(ies_out.number_vertical_angles);
         for (int i = 0; i < ies_out.number_vertical_angles; i++) {
-            NEXT_FLOAT("vertical angles")   ies_out.vertical_angles.push_back(value);
+            NEXT_VALUE("vertical angles")   ies_out.vertical_angles.push_back(value);
             if (value < ies_out.min_vertical_angle) ies_out.min_vertical_angle = value;
             if (value > ies_out.max_vertical_angle) ies_out.max_vertical_angle = value;
         }
@@ -198,7 +205,7 @@ public:
         ies_out.max_horizontal_angle = -360;
         ies_out.horizontal_angles.reserve(ies_out.number_horizontal_angles);
         for (int i = 0; i < ies_out.number_horizontal_angles; i++) {
-            NEXT_FLOAT("horizontal angles") ies_out.horizontal_angles.push_back(value);
+            NEXT_VALUE("horizontal angles") ies_out.horizontal_angles.push_back(value);
             if (value < ies_out.min_horizontal_angle) ies_out.min_horizontal_angle = value;
             if (value > ies_out.max_horizontal_angle) ies_out.max_horizontal_angle = value;
         }
@@ -208,17 +215,17 @@ public:
         ies_out.candela.reserve(static_cast<uint64_t>(ies_out.number_vertical_angles) * static_cast<uint64_t>(ies_out.number_horizontal_angles));
         ies_out.max_candela = 0;
         for (int i = 0; i < ies_out.number_vertical_angles * ies_out.number_horizontal_angles; i++) {
-            NEXT_FLOAT("candela values")    ies_out.candela.push_back(value);
+            NEXT_VALUE("candela values")    ies_out.candela.push_back(value);
             if (ies_out.max_candela < value) ies_out.max_candela = value;
         }
-#undef NEXT_FLOAT
+#undef NEXT_VALUE
         f.close();
         return true;
     }
 
 private:
     /* read property in current line and return value */
-    static inline bool read_property(const std::string& name, std::string& line, std::string& out) {
+    static bool read_property(const std::string& name, std::string& line, std::string& out) {
         size_t pos = line.find(name);
         if (pos == std::string::npos) return false;
 
@@ -229,7 +236,7 @@ private:
         return true;
     }
     /* read property in current line and add it to the dict */
-    static inline bool read_property(std::string& line, std::map<std::string, std::string>& property_map) {
+    static bool read_property(std::string& line, std::map<std::string, std::string>& property_map) {
         const std::string start_delim = "[";
         const std::string stop_delim = "]";
         const size_t attribute_start = line.find(start_delim) + start_delim.size();
