4f0bc87c7e40e1583d295dc6c9805b8129434633
Lukas Lipp <llipp@cg.tuwien.ac.at>
2022-09-01 16:42:10 +0200

    remove IESNA91 not supported warning

--- tiny_ies.hpp
+++ tiny_ies.hpp
@@ -133,11 +133,8 @@ public:
         // the first line in a valid ies file should be IESNA:
         if (f.good()) {
             std::getline(f, line);
-            if (!read_property("IESNA91", line, ies_out.ies_version)) {
-                warn_out = "Version IESNA91 may not be supported: " + file;
-            }
-            else if (!read_property("IESNA:", line, ies_out.ies_version)) {
-                warn_out = "First line did not start with IESNA: " + file;
+        	if (!read_property("IESNA", line, ies_out.ies_version)) {
+                warn_out = "First line did not start with IESNA " + file;
             }
         }
         // read properties
